f2167d16ce4f75a838fc6e7bbd9f68e1b680f930
Nazmul Hossain <nazmulwanted@gmail.com>
2020-04-27 00:19:18 +0600

    Added comoponent folders and setup a basic header part.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <p>Number of friends added: 
+               <br />
+               Yearly Salary in Total: 
+            </p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/userInfo.js
@@ -0,0 +1,109 @@
+var users = [
+    {
+        "name": "Abul",
+        "address": "Wari, Dhaka",
+        "email": "abul@test.com",
+        "phone": "0200001",
+        "yearSalary": 3600,
+    }
+    {
+        "name": "Kabul",
+        "address": "MohaKhali, Dhaka",
+        "email": "kabul@test.com",
+        "phone": "0200002",
+        "yearSalary": 5000,
+    }
+    {
+        "name": "Babul",
+        "address": "Mohammadpur, Dhaka",
+        "email": "babul@test.com",
+        "phone": "0200003",
+        "yearSalary": 3640,
+    }
+    {
+        "name": "Momin",
+        "address": "Shahbag, Dhaka",
+        "email": "momin@test.com",
+        "phone": "0200004",
+        "yearSalary": 3240,
+    }
+    {
+        "name": "Laila",
+        "address": "Shantinogor, Dhaka",
+        "email": "laila@test.com",
+        "phone": "0200005",
+        "yearSalary": 5000,
+    }
+    {
+        "name": "Mojnu",
+        "address": "Maghbazar, Dhaka",
+        "email": "mojnu@test.com",
+        "phone": "0200006",
+        "yearSalary": 6000,
+    }
+    {
+        "name": "Shiri",
+        "address": "Farmgate, Dhaka",
+        "email": "shiri@test.com",
+        "phone": "0200007",
+        "yearSalary": 8980,
+    }
+    {
+        "name": "Farhad",
+        "address": "Kamlapur, Dhaka",
+        "email": "farhad@test.com",
+        "phone": "0200008",
+        "yearSalary": 7890,
+    }
+    {
+        "name": "Radha",
+        "address": "Mughdhapara, Dhaka",
+        "email": "radha@test.com",
+        "phone": "0200009",
+        "yearSalary": 5430,
+    }
+    {
+        "name": "Krishna",
+        "address": "Khilkhet, Dhaka",
+        "email": "krishna@test.com",
+        "phone": "0200010",
+        "yearSalary": 4500,
+    }
+    {
+        "name": "Salam",
+        "address": "Cantonment, Dhaka",
+        "email": "salam@test.com",
+        "phone": "0200011",
+        "yearSalary": 6700,
+    }
+    {
+        "name": "Kalam",
+        "address": "Vatara, Dhaka",
+        "email": "kalam@test.com",
+        "phone": "0200012",
+        "yearSalary": 5600,
+    }
+    {
+        "name": "Hasan",
+        "address": "Boshundhara, Dhaka",
+        "email": "hasan@test.com",
+        "phone": "0200013",
+        "yearSalary": 3600,
+    }
+    {
+        "name": "Kabir",
+        "address": "Lalbagh, Dhaka",
+        "email": "kabir@test.com",
+        "phone": "0200014",
+        "yearSalary": 3400,
+    }
+    {
+        "name": "Abdul",
+        "address": "Uttara, Dhaka",
+        "email": "abdul@test.com",
+        "phone": "0200015",
+        "yearSalary": 3600,
+    }
+];
+
+export default users;
